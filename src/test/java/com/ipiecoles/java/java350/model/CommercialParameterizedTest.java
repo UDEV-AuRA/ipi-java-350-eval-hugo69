11463a21e4f9a1cb4015f842255cf260926145f5
hugo69 <hugo.charbonneyriat@gmail.com>
2018-03-12 11:51:55 +0100

    test fiabilité unit junit parameters

--- src/test/java/com/ipiecoles/java/java350/model/CommercialParameterizedTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/CommercialParameterizedTest.java
@@ -2,21 +2,47 @@ package com.ipiecoles.java.java350.model;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 /**
  * Created by HCHARBONNEYR on 12/03/2018.
  */
-public class CommercialParameterizedTest {
+    @RunWith(value = Parameterized.class)
+    public class CommercialParameterizedTest {
+
+        @Parameter(value = 0)
+        public Integer perf;
+        @Parameter(value = 1)
+        public Note expectedNote;
+
+        @Parameterized.Parameters(name = "perf {0} est équivalent à : {1}")
+        public static Collection<Object[]> data(){
+            return Arrays.asList(new Object[][]{
+                    {0 , Note.INSUFFISANT},
+                    {50, Note.INSUFFISANT},
+                    {100, Note.PASSABLE},
+                    {150, Note.BIEN},
+                    {200, Note.TRES_BIEN},
+                    {null,null},
+                    {600,null},
+            });
+        }
 
     @Test
-    public void testPrimeAnnuelleWithCANull(){
-        //Given initialisation des données d'entrée
+    public void testCheckCommercialNote(){
+        //Given
+        Commercial commercial = new Commercial();
+        commercial.setPerformance(perf);
 
-        //When Exécution/test de la méthode
+        //when
+        Note note = commercial.equivalenceNote();
 
-        //Then vérification par rapport à la sortie de la méthode
-
-
+        //then
+        Assertions.assertThat(note).isEqualTo(expectedNote);
     }
-
 }
