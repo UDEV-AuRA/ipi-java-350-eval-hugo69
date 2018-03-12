6834800c831ee05700047766e18e72b1034dcaff
hugo69 <hugo.charbonneyriat@gmail.com>
2018-03-12 14:15:37 +0100

    repository test 2

--- src/test/java/com/ipiecoles/java/java350/model/repository/EmployeRepositoryTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/repository/EmployeRepositoryTest.java
@@ -22,21 +22,36 @@ import java.util.List;
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @SpringBootTest(classes = MySpringApplication.class)
-public class EmployeRepositoryTest {
+public class
+EmployeRepositoryTest {
 
     @Autowired
     private EmployeRepository employeRepository;
 
     @Test
-    public void test(){
+    public void testfindByNomOrPrenomAllIgnoreCasePrenom(){
         //given
-
         Commercial c = new Commercial();
         c.setPrenom("Pierre");
         c.setNom("Durand");
         c = employeRepository.save(c);
         //when
         List<Employe> employeList = employeRepository.findByNomOrPrenomAllIgnoreCase("pierre");
+        //then
+        Assertions.assertThat(employeList).isEmpty();
+        Assertions.assertThat(employeList).hasSize(1);
+        Assertions.assertThat(employeList).contains(c);
+    }
+    @Test
+
+    public void testFindByNomOrPrenomAllIgnoreCaseNom(){
+        //given
+        Commercial c = new Commercial();
+        c.setPrenom("Pierre");
+        c.setNom("Durand");
+        c = employeRepository.save(c);
+        //when
+        List<Employe> employeList = employeRepository.findByNomOrPrenomAllIgnoreCase("durand");
         //then
         Assertions.assertThat(employeList).isEmpty();
         Assertions.assertThat(employeList).hasSize(1);
