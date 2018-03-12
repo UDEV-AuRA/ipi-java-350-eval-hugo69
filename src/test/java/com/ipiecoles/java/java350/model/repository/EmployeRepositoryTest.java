b9bcd28a9ef6421d5825a4db5012e20e467c3238
hugo69 <hugo.charbonneyriat@gmail.com>
2018-03-12 12:56:04 +0100

    repository test 2

---
+++ src/test/java/com/ipiecoles/java/java350/model/repository/EmployeRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.ipiecoles.java.java350.model.repository;
+
+import com.ipiecoles.java.java350.MySpringApplication;
+import com.ipiecoles.java.java350.model.Commercial;
+import com.ipiecoles.java.java350.model.Employe;
+import com.ipiecoles.java.java350.repository.EmployeRepository;
+import org.assertj.core.api.Assertions;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+/**
+ * Created by HCHARBONNEYR on 12/03/2018.
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@SpringBootTest(classes = MySpringApplication.class)
+public class EmployeRepositoryTest {
+
+    @Autowired
+    private EmployeRepository employeRepository;
+
+    @Test
+    public void test(){
+        //given
+
+        Commercial c = new Commercial();
+        c.setPrenom("Pierre");
+        c.setNom("Durand");
+        c = employeRepository.save(c);
+        //when
+        List<Employe> employeList = employeRepository.findByNomOrPrenomAllIgnoreCase("pierre");
+        //then
+        Assertions.assertThat(employeList).isEmpty();
+        Assertions.assertThat(employeList).hasSize(1);
+        Assertions.assertThat(employeList).contains(c);
+    }
+}
